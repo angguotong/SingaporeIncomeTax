fd8f022cb71057f28ac849c2c411074e370df608
shawn <guotong.ang@grabtaxi.com>
2022-07-14 14:41:55 +0800

    Singapore income tax calculator

---
+++ incometax.py
@@ -0,0 +1,33 @@
+
+
+def incometax(income):
+
+    if not isinstance(income,float):
+        print("please input with decimal")
+
+
+    incomeRange = [20,10,10,40,40,40,40,40,40,40,180,500]
+    taxRange = [0,2,3.5,7,11.5,15,18,19,19.5,20,22,23]
+
+    income /= 1000
+    taxableIncome = 0
+    for i in range(len(incomeRange)):
+        print(income, taxableIncome)
+
+        if income - incomeRange[i] <= 0: 
+            taxableIncome += income * (taxRange[i]/100)
+            return taxableIncome*1000
+
+
+        elif income - incomeRange[i] > 0:
+            print (incomeRange[i],taxRange[i])
+            taxableIncome += incomeRange[i] * (taxRange[i]/100) if incomeRange[i] - income < 0 else (income-incomeRange[i]) * (taxRange[i]/100)
+            income = income - incomeRange[i]
+    return taxableIncome*1000
+
+taxableIncome = incometax(67200.00)
+
+print (taxableIncome)
+
+            
+
